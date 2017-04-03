069751fdffb70f66cf36b97c396d64ae265c8dc2
Brad Sherman <bsherman1096@gmail.com>
2017-04-03 17:51:57 -0400

    fix folder issues

---
+++ SafetyMaintenance/src/safetymaintenance/parts/ReadXMLFile.java
@@ -0,0 +1,54 @@
+package safetymaintenance.parts;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import java.io.File;
+
+public class ReadXMLFile {
+
+  public static void main(String argv[]) {
+
+    try {
+
+	File fXmlFile = new File("../data/Assumption.xml");
+	DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+	DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+	Document doc = dBuilder.parse(fXmlFile);
+
+	//optional, but recommended
+	//read this - http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
+	doc.getDocumentElement().normalize();
+
+	System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+
+	NodeList nList = doc.getElementsByTagName("Assumption");
+
+	System.out.println("----------------------------");
+
+	for (int temp = 0; temp < nList.getLength(); temp++) {
+
+		Node nNode = nList.item(temp);
+
+		System.out.println("\nCurrent Element :" + nNode.getNodeName());
+
+		if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+			Element eElement = (Element) nNode;
+
+			System.out.println("ID : " + eElement.getAttribute("ID"));
+			System.out.println("Description : " + eElement.getAttribute("Description"));
+
+		}
+	}
+    } catch (Exception e) {
+    	e.printStackTrace();
+    }
+  }
+
+}
+
