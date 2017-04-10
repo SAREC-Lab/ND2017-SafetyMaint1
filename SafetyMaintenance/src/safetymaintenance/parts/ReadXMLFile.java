c1e8932145c274c42acc256157391cb58fd70dbb
jfrio <jfrio@nd.edu>
2017-04-10 17:23:46 -0400

    Added Code to read in TM Files

--- SafetyMaintenance/src/safetymaintenance/parts/ReadXMLFile.java
+++ SafetyMaintenance/src/safetymaintenance/parts/ReadXMLFile.java
@@ -13,19 +13,30 @@ public class ReadXMLFile {
 
   public static void main(String argv[]) {
 
-	List<String> fileNames = new ArrayList<String>();
-	fileNames.add("Assumption");
-	fileNames.add("DesignDecision");
-	fileNames.add("FMECA");
-	fileNames.add("Goal");
-	fileNames.add("Requirement");
+	List<String> xmlFileNames = new ArrayList<String>();
+	xmlFileNames.add("Assumption");
+	xmlFileNames.add("DesignDecision");
+	xmlFileNames.add("FMECA");
+	xmlFileNames.add("Goal");
+	xmlFileNames.add("Requirement");
+
+	List<String> tmFileNames = new ArrayList<String>();
+	tmFileNames.add("Code_Requirements");   //TMReqCode
+	tmFileNames.add("Requirements_Assumptions"); //TMAssumption
+	tmFileNames.add("Requirements_Design"); //TMDesign
+	tmFileNames.add("Requirements_Fault"); //TMFault
+	
+	List<String> tmTagNames = new ArrayList<String>();
+	tmTagNames.add("TMReqCode");
+	tmTagNames.add("TMAssumption");
+	tmTagNames.add("TMDesign");
+	tmTagNames.add("TMFault");
 	
 	List<myNode> allNodes = new ArrayList<myNode>();
-
 	
     try {
-    	for(int i=0; i<fileNames.size(); i++){
-		    File fXmlFile = new File("data/" + fileNames.get(i) + ".xml");
+    	for(int i=0; i<xmlFileNames.size(); i++){
+		    File fXmlFile = new File("data/" + xmlFileNames.get(i) + ".xml");
 			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 			Document doc = dBuilder.parse(fXmlFile);
@@ -36,7 +47,7 @@ public class ReadXMLFile {
 		
 			//System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
 		
-			NodeList nList = doc.getElementsByTagName(fileNames.get(i));
+			NodeList nList = doc.getElementsByTagName(xmlFileNames.get(i));
 		
 			//System.out.println("----------------------------");
 		
@@ -57,7 +68,7 @@ public class ReadXMLFile {
 					currentNode.setDescription(eElement.getElementsByTagName("Description").item(0).getTextContent());
 					//System.out.println("ID : " + eElement.getElementsByTagName("ID").item(0).getTextContent());
 					//System.out.println("Description : " + eElement.getElementsByTagName("Description").item(0).getTextContent());
-					if(fileNames.get(i) == "FMECA"){
+					if(xmlFileNames.get(i) == "FMECA"){
 						currentNode.setCriticality(eElement.getElementsByTagName("Criticality").item(0).getTextContent());
 						//System.out.println("Criticality : " + eElement.getElementsByTagName("Criticality").item(0).getTextContent());
 					}
@@ -75,7 +86,35 @@ public class ReadXMLFile {
     	System.out.println("Node: " + i.getID());
     }
   
-  
+    try {    	
+    	for(int i=0; i<tmFileNames.size(); i++){
+		    File fXmlFile = new File("data/TM_" + tmFileNames.get(i) + ".xml");
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(fXmlFile);
+		
+			//optional, but recommended
+			//read this - http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
+			doc.getDocumentElement().normalize();
+				
+			NodeList nList = doc.getElementsByTagName(tmTagNames.get(i));
+			
+			for (int temp = 0; temp < nList.getLength(); temp++) {
+				Node nNode = nList.item(temp);				
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+		
+					Element eElement = (Element) nNode;
+		
+					System.out.println("ID : " + eElement.getElementsByTagName("ID").item(0).getTextContent());
+					System.out.println("Description : " + eElement.getElementsByTagName("Description").item(0).getTextContent());
+					
+				}
+			}
+    	}
+    } catch (Exception e) {
+    	e.printStackTrace();
+    }
+    
   }
 
 }
